2eac4f9c3b5f19c77b10ccc82ec3e26e4d737919
Wendy Nguyen <phucnguyen@nvg.vn>
2019-10-18 15:19:11 +0700

    n

--- Tests/BVT/SM01_Login.js
+++ Tests/BVT/SM01_Login.js
@@ -16,7 +16,7 @@ describe('SM01 - Login', function() {
   */
   it.skip('Verify user can login successful with valid account', function() {
     this.accounts.forEach((account) => {
-      // Login with valid account hhh
+      // Login with valid 
       cy.login(account.email, account.password)
 
       // VP: username displays on my profile page
